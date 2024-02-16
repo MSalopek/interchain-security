cc88d016c01495e0e3113b8564116cb10391ad07
MSalopek <matija.salopek994@gmail.com>
2024-02-16 19:56:22 +0100

    adding some printlines

--- docs/build_deploy.sh
+++ docs/build_deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+echo "------ STARTING BUILD  -------"
 # pull in the versions from versions.json
-source ./sync_versions.sh
 
-# echo "building docusaurus main docs"
-# npm ci && npm run build
-# cp -r ./legacy ./build/
-# mv build ~/output
-# echo "done building docusaurus main docs"
+echo "building docusaurus main docs"
+npm ci && npm run build
+cp -r ./legacy ./build/
+mv build ~/output
+echo "done building docusaurus main docs"
