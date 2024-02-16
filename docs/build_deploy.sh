8a769b63281267f721e751321b37f4e93e584ee4
MSalopek <matija.salopek994@gmail.com>
2024-02-16 19:53:10 +0100

    just deploy

--- docs/build_deploy.sh
+++ docs/build_deploy.sh
@@ -3,8 +3,8 @@
 # pull in the versions from versions.json
 source ./sync_versions.sh
 
-echo "building docusaurus main docs"
-npm ci && npm run build
-cp -r ./legacy ./build/
-mv build ~/output
-echo "done building docusaurus main docs"
+# echo "building docusaurus main docs"
+# npm ci && npm run build
+# cp -r ./legacy ./build/
+# mv build ~/output
+# echo "done building docusaurus main docs"
