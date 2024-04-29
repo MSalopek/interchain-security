ab63d85cc7779765e8f1b2db82d10d2b01af9828
MSalopek <matija.salopek994@gmail.com>
2024-04-29 21:12:32 +0200

    experiment: change pull cmd in build docs

--- docs/build_deploy.sh
+++ docs/build_deploy.sh
@@ -8,8 +8,8 @@ echo "######### BUILD DOCS #########"
 echo "building docusaurus main docs"
 # npm ci && npm run build
 # copy legacy docs to build folder
-git fetch origin legacy-docs-page
-git checkout origin/legacy-docs-page -- legacy
+git fetch origin legacy-docs-page:legacy-docs-page
+git checkout legacy-docs-page -- legacy
 cp -r ../legacy ./build/
 mv build ~/output
 echo "done building docusaurus main docs"
