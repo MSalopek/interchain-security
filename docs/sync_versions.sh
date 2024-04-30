842db7012c3de220f1e744dbeca08a408f69f3bf
MSalopek <matija.salopek994@gmail.com>
2024-04-30 15:24:31 +0200

    try adding tag instead of branch for legacy

--- docs/sync_versions.sh
+++ docs/sync_versions.sh
@@ -11,7 +11,8 @@ for version in $(jq -r .[] versions.json); do
     rm versions.json package-lock.json
 done
 
-git checkout v3-docs-legacy -- legacy
+git fetch origin
+git checkout origin/v3-docs-legacy -- legacy
 
 # return to initial branch but keep the files created by Docusarus in the loop above
 (git reset --hard && git checkout $COMMIT)
