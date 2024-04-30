994d0840f69f1d3481c230d99318af17fc065ea9
MSalopek <matija.salopek994@gmail.com>
2024-04-30 15:16:53 +0200

    try adding tag instead of branch for legacy

--- docs/build_deploy.sh
+++ docs/build_deploy.sh
@@ -5,7 +5,7 @@ echo "######### BUILD DOCS #########"
 echo "building docusaurus main docs"
 npm ci && npm run build
 # copy legacy docs to build folder
-git fetch v3-docs-legacy
+git fetch origin v3-docs-legacy
 git checkout v3-docs-legacy -- legacy
 cp -r ./legacy ./build/
 mv build ~/output
